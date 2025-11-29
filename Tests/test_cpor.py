4402bb7fa78599b5ddb6f9c1925bf92eba5918cc
Guy Azran <guy.azran@campus.technion.ac.il>
2025-11-29 02:39:20 +0200

    more domains in tests

--- Tests/test_cpor.py
+++ Tests/test_cpor.py
@@ -1,14 +1,35 @@
+import os
+# set PYTHONNET_RUNTIME=mono
+os.environ["PYTHONNET_RUNTIME"] = "mono"
+
+# set PYTHONNET_MONO_LIBMONO="$(brew --prefix mono)/lib/libmonosgen-2.0.dylib"
+os.environ["PYTHONNET_MONO_LIBMONO"] = "/opt/homebrew/opt/mono/lib/libmonosgen-2.0.dylib"
+
 from unified_planning.io import PDDLReader
 import unified_planning.environment as environment
 from unified_planning.engines.results import PlanGenerationResultStatus
 from unified_planning.shortcuts import *
+
 
 if __name__ == "__main__":
 
     # Creating a PDDL reader
     reader = PDDLReader()
 
-    prob_arr = ['blocks2', 'blocks3', 'doors5', 'wumpus05']
+    prob_arr = [
+        'blocks2',
+        'blocks3',
+        'blocks7',
+        # 'colorballs2-2',  # PDDL parsing fails
+        'doors5',
+        # 'doors15',  # stuck after planning complete
+        'localize5',
+        # 'localize5noisy',  # PDDL parsing fails
+        # 'medpks010',  # PDDL parsing fails
+        'unix1',
+        'wumpus05',
+        # 'wumpus10'  # PDDL parsing fails
+    ]  
 
     for prob in prob_arr:
         print(f"###########################Problem: {prob} start###########################")
@@ -28,3 +49,4 @@ if __name__ == "__main__":
                 print(f'Success')
             else:
                 print('No plan found!')
+                assert False, f'Failed on problem {prob}'
